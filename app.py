30a32b88fb9d0b1ed33619f28ed83f7996cb7f35
Koijikun <barbosan@students.zhaw.ch>
2024-05-22 13:24:24 +0200

    Anpassung app.py

--- app.py
+++ app.py
@@ -1,52 +1,33 @@
 import streamlit as st
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# streamlit cache clear (or menu)
-@st.cache_resource
+@st.cache_data
 def load_data():
-    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-    model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
-    return tokenizer, model
+    data = pd.read_csv("https://people.sc.fsu.edu/~jburkardt/data/csv/hw_200.csv")
+    return data
 
-st.set_page_config(page_title="Chatbot")
-st.write("Welcome to the DialoGPT-medium chatbot example.")
-tokenizer, model = load_data()
+data = load_data()
 
-# Init Session State
-if 'step' not in st.session_state:
-    st.session_state.step = 0
-    st.session_state.chat_history_ids = None
-    st.session_state.history = []
+st.title("MDM-Streamlit")
+
+display_type = st.radio("Wie sollen die Daten dargestellt werden?", ("Tabelle", "Grafik"))
+
+row_range = st.slider("Wieviele Datensätze sollen angezeigt werden?", 0, len(data)-1, (0, 100))
+
+filtered_data = data.iloc[row_range[0]:row_range[1]]
+
+if 'Index' in filtered_data.columns:
+    filtered_data = filtered_data.drop(columns=['Index'])
+
+if display_type == "Tabelle":
+    st.write("Tabelle:")
+    st.write(filtered_data)
 else:
-    st.session_state.step+=1
-
-input = st.text_input(label="Enter your question:")
-
-if input:
-    # encode the new user input, add the eos_token and return a tensor in Pytorch
-    new_user_input_ids = tokenizer.encode(input + tokenizer.eos_token, return_tensors='pt')
-
-    # append the new user input tokens to the chat history
-    bot_input_ids = torch.cat([st.session_state.chat_history_ids, new_user_input_ids], dim=-1) if st.session_state.step > 1 else new_user_input_ids
-
-    # generated a response while limiting the total chat history to 1000 tokens, 
-    st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-    # pretty print last ouput tokens from bot
-    response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-    st.session_state.history.append(input)
-    st.session_state.history.append(response)
-
-if st.button('Reset'):
-    st.session_state.step = 0
-    st.session_state.chat_history_ids = None
-    st.session_state.history = []
-
-for x in range(len(st.session_state.history)):
-    person = "**User**" if x%2==0 else "**Bot**"
-    st.write(person, st.session_state.history[x])
-    
-st.write("Step: ", st.session_state.step)
-st.write(st.session_state.chat_history_ids)
-
+    st.write("Grafik:")
+    fig, ax = plt.subplots()
+    ax.set_xlabel("Grösse (Inches)") 
+    ax.set_ylabel("Gewicht (Pounds)") 
+    sns.scatterplot(data=filtered_data, x=data.columns[2], y=data.columns[1], ax=ax)
+    st.pyplot(fig)
